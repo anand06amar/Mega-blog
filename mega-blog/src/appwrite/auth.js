f5a041d749b1b2a3c28458b10797e4955590fd3f
anand06amar <anand07amar@gmail.com>
2024-05-25 23:46:36 +0530

    added authentication service with appwrite

---
+++ mega-blog/src/appwrite/auth.js
@@ -0,0 +1,75 @@
+import config from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+export class Authservice {
+
+    client = new Client()
+    account;
+
+    constructor(){
+        this.client
+        .setEndpoint(config.appwriteUrl) 
+    .setProject(config.appwriteProjectID);
+    this.account = new Account(this.client)
+    }
+
+
+    async createAccount({email,password,name}){
+        try {
+            const userAccount= await this.account.create(ID.unique(),email, password, name);
+            if (userAccount){
+                //another method
+                return
+            }
+            else{
+                return userAccount;
+            }
+            
+        } catch (error) {
+            throw error;
+            
+        }
+
+    }
+
+    async login({email,password}){
+        try {
+            return await this.account.createEmailPasswordSession(email,password);
+
+        } catch (error) {
+            throw error;
+        }
+
+    }
+
+    async logout(){
+        try {
+             await this.account.deleteSessions();
+
+
+        } catch (error) {
+            throw error;
+        }
+
+
+
+
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+    }
+
+}
+
+const authservice = new Authservice()
+
+
+
+
+export default authservice;
+
--- mega-blog/src/config/config.js
+++ mega-blog/src/config/config.js
@@ -3,7 +3,6 @@ appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
 appwriteProjectID: String(import.meta.env.VITE_PROJECT_ID),
 appwriteDatabaseID: String(import.meta.env.VITE_DATABASE_ID),
 appwriteCollectionID: String(import.meta.env.VITE_COLLECTION_ID),
-appwriteDatabaseID: String(import.meta.env.VITE_DATABASE_ID),
 appwriteBucketID: String(import.meta.env.VITE_BUCKET_ID),
 
 
